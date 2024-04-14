c008a2d2451b9f4acc1e7b49722c529fb4f19386
imranabdisalan7 <agmraan@gmail.com>
2024-04-14 23:28:53 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('create-task-form');
+  
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+  
+        const taskInput = document.getElementById('new-task-description');
+        const taskDescription = taskInput.value;
+  
+        const taskItem = document.createElement('li');
+  
+        taskItem.textContent = taskDescription;
+  
+        const taskList = document.getElementById('tasks');
+        taskList.appendChild(taskItem);
+  
+        taskInput.value = '';
+    });
+  });
 });
